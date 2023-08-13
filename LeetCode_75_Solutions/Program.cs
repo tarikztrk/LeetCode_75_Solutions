4c808755403d85e0b373e4ee91559ca2dd4975db
Tarık Öztürk <ozturktariik@gmail.com>
2023-08-13 23:07:49 +0300

    Add project files.

---
+++ LeetCode_75_Solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode_75_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
