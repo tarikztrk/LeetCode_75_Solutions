80924afb3bf0c49f571cf1964ab6d2f795576ca9
Tarık Öztürk <ozturktariik@gmail.com>
2023-08-14 16:05:08 +0300

    KidsWithCandiesSolution added

---
+++ LeetCode_75_Solutions/KidsWithCandies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_75_Solutions
+{
+    public class KidsWithCandiesSolution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            IList<bool> res = new List<bool>();
+            int max = candies.Max();
+
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= max)
+                {
+                    res.Add(true);
+                }
+                else
+                {
+                    res.Add(false);
+                }
+            }
+            return res;
+        }
+    }
+}
