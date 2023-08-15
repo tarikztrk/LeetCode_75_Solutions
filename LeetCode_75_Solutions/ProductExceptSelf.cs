f6cc416f2ed7204e66cdb2325f3efa0f1562e598
Tarık Öztürk <ozturktariik@gmail.com>
2023-08-15 13:29:05 +0300

    2 more solutions added

---
+++ LeetCode_75_Solutions/ProductExceptSelf.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_75_Solutions
+{
+    public class ProductExceptSelfSolution
+    {
+        //https://leetcode.com/problems/product-of-array-except-self/
+        //238. Product of Array Except Self
+        //Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the
+        //elements of nums except nums[i].
+        //Example:
+        //Input:  [1,2,3,4]
+        //Output: [24,12,8,6]
+        
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int zeroCount = 0;
+            int productZero = 1;
+            int product = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    product *= nums[i];
+
+                }
+                if (nums[i] == 0) { zeroCount++; }
+                productZero *= nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0 && zeroCount == 1)
+                {
+                    result[i] = product;
+                }
+                else if (nums[i] == 0 && zeroCount > 1)
+                {
+                    result[i] = 0;
+                }
+                else
+                {
+                    result[i] = productZero / nums[i];
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ LeetCode_75_Solutions/ReverseWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_75_Solutions
+{
+    //https://leetcode.com/problems/reverse-words-in-a-string/
+    //151. Reverse Words in a String
+    //Given an input string, reverse the string word by word.
+    //Example 1:
+    //Input: "the sky is blue"
+    //Output: "blue is sky the"
+    //Example 2:
+    //Input: "  hello world!  "
+    //Output: "world! hello"
+    //Explanation: Your reversed string should not contain leading or trailing spaces.
+    //Example 3:
+    //Input: "a good   example"
+    //Output: "example good a"
+    //Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+    public class ReverseWordsSolution
+    {
+        public string ReverseWords(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            s = string.Join(" ", s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] splittedWords = s.Split(' ').Reverse().ToArray();
+            for (int i = 0; i < splittedWords.Length; i++)
+            {
+                sb.Append(splittedWords[i]); sb.Append(" ");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+    }
+}
