f46395b061281fc80522ee79fb451dcddb34fa38
Tarık Öztürk <ozturktariik@gmail.com>
2023-08-16 18:42:39 +0300

    issubsequence added

---
+++ LeetCode_75_Solutions/IsSubsequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_75_Solutions
+{
+    public class IsSubsequenceSolution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            bool res = true;
+            var a = s.Distinct().ToArray();
+            foreach (var x in a)
+            {
+                if (!t.Contains(x))
+                {
+                    res = false;
+                }
+            }
+            return res;
+        }
+    }
+}
--- LeetCode_75_Solutions/Program.cs
+++ LeetCode_75_Solutions/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LeetCode_75_Solutions
+﻿using System.Text;
+
+namespace LeetCode_75_Solutions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IsSubsequenceSolution increasingTripletSolution = new IsSubsequenceSolution();
+            increasingTripletSolution.IsSubsequence("absasssc", "ahbgdc");
+            Console.WriteLine();
         }
+
+        
     }
 }
