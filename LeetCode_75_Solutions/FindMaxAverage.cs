d814fa9e8a4e436a6bea646b4c4fab9a1079aa7f
Tarık Öztürk <ozturktariik@gmail.com>
2023-08-18 18:47:43 +0300

    find max average solution added

---
+++ LeetCode_75_Solutions/FindMaxAverage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_75_Solutions
+{
+    public class FindMaxAverageSolution
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            
+            double sum = nums.Take(k).Sum();
+            double res = sum;
+            for (int i = k; i < nums.Length; i++)
+            {
+                sum += nums[i] - nums[i-k];
+                res = Math.Max(res, sum);
+            }
+            return res/k;
+        }
+    }
+
+}
